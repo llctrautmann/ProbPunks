5aaf72ff99fff330598ececfb8ef386cfb36b98b
Luca Trautmann <guy_montag@posteo.net>
2023-11-20 16:38:47 +0100

    Change parameters

--- src/params.py
+++ src/params.py
@@ -5,10 +5,10 @@ import torch
 
 @dataclasses.dataclass
 class Params:
-    learning_rate: float = 5e-4
+    learning_rate: float = 3e-4
     beta: float = 0.001
-    epochs: int = 150
-    batch_size: int = 64
+    epochs: int = 50
+    batch_size: int = 32
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     im_width: int = 256
     im_height: int = 256
