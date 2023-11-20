22d4df761d6c688b637496283018cc9c797f0fac
Luca Trautmann <guy_montag@posteo.net>
2023-11-20 15:35:14 +0100

    bug fix

--- src/utils.py
+++ src/utils.py
@@ -34,7 +34,7 @@ def log_to_wandb(train_loss,val_loss, ground_truth, reconstructions, random_imag
     rec_images = torchvision.utils.make_grid(reconstructions[:8,...])
     random_images = torchvision.utils.make_grid(random_images[:8,...])
 
-    wandb.log({"Training loss": loss.item()})
+    wandb.log({"Training loss": train_loss.item()})
     wandb.log({"Validation loss": val_loss.item()})
     wandb.log({"Original": wandb.Image(org_images, caption="Original Input")})
     wandb.log({"Reconstruction": wandb.Image(rec_images, caption="Reconstruction")})
