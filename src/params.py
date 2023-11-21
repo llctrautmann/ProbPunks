de93f8bbdcd9053a357924ce2dbc1feafaab9787
Luca Trautmann <guy_montag@posteo.net>
2023-11-21 09:01:46 +0100

    Change GPU

--- src/params.py
+++ src/params.py
@@ -9,7 +9,7 @@ class Params:
     beta: float = 0.5
     epochs: int = 100
     batch_size: int = 64
-    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    device: str = "cuda:3" if torch.cuda.is_available() else "cpu"
     im_width: int = 128
     im_height: int = 128
 
