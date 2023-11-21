5b50472570d0f4f0c47c13bcd058c34b2cec77f4
Luca Trautmann <guy_montag@posteo.net>
2023-11-21 09:00:46 +0100

    Change beta

--- src/params.py
+++ src/params.py
@@ -6,8 +6,8 @@ import torch
 @dataclasses.dataclass
 class Params:
     learning_rate: float = 3e-4
-    beta: float = 0.1
-    epochs: int = 500
+    beta: float = 0.5
+    epochs: int = 100
     batch_size: int = 64
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     im_width: int = 128
